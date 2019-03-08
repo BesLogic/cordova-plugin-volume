bc067ceec5047363e3cb6c0da91ff08e2d7a84ee
Carl Montplaisir <carl.montplaisir@beslogic.com>
2019-03-07 21:34:51 -0500

    Fix javascript mapping

--- www/VolumeManager.js
+++ www/VolumeManager.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'VolumeManager', 'coolMethod', [arg0]);
+exports.getVolume = function (arg0, success, error) {
+    exec(success, error, 'VolumeManager', 'getVolume', []);
+};
+
+exports.setVolume = function (arg0, success, error) {
+    exec(success, error, 'VolumeManager', 'setVolume', [arg0]);
 };
